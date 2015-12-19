293fc4caa4954526c490a1132978607d3faf266b
Dima Kurilchenko <dima.kurilchenko@gmail.com>
2015-12-19 16:02:28 +0300

    Initial stuff

---
+++ Assets/Scripts/Visitor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Visitor : MonoBehaviour
+{
+
+    public Transform cameraRig;
+
+    void Start()
+    {
+        if (UnityEngine.VR.VRSettings.enabled)
+        {
+            cameraRig.gameObject.GetComponent<MouseCameraControl>().enabled = false;
+        }
+        else
+        {
+            cameraRig.gameObject.GetComponent<MouseCameraControl>().enabled = true;
+        }
+    }
+
+}
